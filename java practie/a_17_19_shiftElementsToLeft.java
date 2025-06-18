698c3fb4a88e98f5ca809061aa5f49bf083c6f8a
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2025-06-19 00:19:25 +0530

    .

---
+++ java practie/a_17_19_shiftElementsToLeft.java
@@ -0,0 +1,20 @@
+//  Shift all elements to the left by one index.
+
+
+
+public class a_17_19_shiftElementsToLeft {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        int left = arr[0];
+
+
+        for (int i = 1; i < arr.length; i++) {
+            int crrElement = arr[i];
+            left = crrElement;
+        }
+
+        System.out.print(arr[]);
+
+        // CHANGES NEED TO BE MADE
+    }
+}
