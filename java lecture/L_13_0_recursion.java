d1ed306677eea9a7a60c7fbc79ed6c1cafeaa27d
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2025-07-13 15:08:55 +0530

    .

---
+++ java lecture/L_13_0_recursion.java
@@ -0,0 +1,34 @@
+public class L_13_0_recursion {
+    public static void printNum1(int a) {
+        // printing the nums 5 to 1
+        if (a == 0) {
+            return;
+        }
+        System.out.print(a + " ");
+        printNum1(a - 1); // it will keep callling the "printNum" with decreasing the value by 1, untion a == 0. 
+    }
+
+    public static void printNum2(int a) {
+        // printing nums 1 to 10
+        if (a == 11) {
+            return;
+        }
+
+        System.out.print(a+" ");
+        printNum2(a+1);
+    }
+
+    public static void main(String[] args) {
+        // printing numbers form 5 to 1
+
+        int n = 5;
+        System.out.print("printNum1: ");
+        printNum1(n);
+        System.out.println();
+
+        System.out.print("printNum2: ");
+        printNum2(1);
+
+    }
+
+}
---
+++ java practie/a_13_0_sumOfFirstNNaturalNums.java
@@ -0,0 +1,15 @@
+// sum of first n natural numbers
+
+public class a_13_0_sumOfFirstNNaturalNums {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void sum() {
+        int a=0;
+
+        int sum = (a+1);
+        
+
+    }
+}
